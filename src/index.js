d2db390acb4e4e49ce5addd6e01bd3489ff1aafd
Dixit <satyam_dixit@optum.com>
2023-07-06 11:44:44 +0530

    End of section 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
-import App from './app/layout/App';
+import App from './app/layout/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
